bcd2ca663ae1cd61516c02a01cb14764c96066c1
lewe0fun <pakuln.yury@gmail.com>
2023-09-12 00:16:49 +0300

    hw1

--- src/test/java/AssertExamples.java
+++ src/test/java/AssertExamples.java
@@ -1,6 +1,6 @@
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class Main {
+public class AssertExamples {
     public static void main(String[] args) {
         String text = "Hello, World";
 
